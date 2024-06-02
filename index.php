e090081915cb078c735736334120233e0c502c35
emily <emymi2007@gmail.com>
2024-06-02 13:35:24 -0300

    Adicionei meu perfil do linkedin e arrumei  cabeçalho

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
    
 
 </head>
-<body class="d-flex h-100 text-center text-bg-dark">
+<body class="d-flex flex-column h-100 text-center text-white" style="background-color: #212529;">
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
       <header class="mb-auto">
         <div>
-          <h3 class="float-md-start mb-0">Aplicação PHP</h3>
+          <h3 class="float-md-start mb-0 text-white">Aplicação PHP</h3>
           <nav class="nav nav-masthead justify-content-center float-md-end">
             <a class="nav-link active" aria-current="page" href="index.php">Home</a>
             <a class="nav-link" href="login.php">Login</a>
@@ -24,13 +24,14 @@
 
       <main class="px-3">
         <h1>Bem-vindo à aplicação PHP.</h1>
-        <p class="lead">Esta é a página principal. Utilize o menu acima para navegar.</p>
-        <a href="https://github.com/emilypessoa" class="btn btn-primary">GitHub</a>
+        <p class="lead">Este é um template obtido de Bootstrap 5. Você pode personalizá-lo conforme necessário.</p>
+        <a href="https://github.com/emilypessoa" class="btn btn-primary">Veja meu GitHub</a>
       </main>
 
-      <footer class="mt-auto text-white-50">
-        <p>Aplicação PHP com Bootstrap 5, by <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>.</p>
-      </footer>
+      <footer class="mt-auto text-white-50" style="background-color: #212529; padding: 20px;">
+        <p style="color: white;">Aplicação PHP com Bootstrap 5, by <a href="https://getbootstrap.com/" style="color: white;">Bootstrap</a>.</p>
+        <p style="color: white;">Projeto criado por <a href="https://www.linkedin.com/in/emily-rodrigues-pessoa-186a93268?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" style="color: white;">@emilyrodriguespessoa</a>.</p>
+    </footer>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+9b5U5fR5z0d3N5XyFw5p6C5MQf6D" crossorigin="anonymous"></script>
   </body>
