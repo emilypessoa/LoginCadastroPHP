82f7cdd8efae1af6830162659c8b8dc7333d97e6
emily <emymi2007@gmail.com>
2024-06-02 13:10:54 -0300

    head index  com o bootstrap

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ENjdO4Dr2bkBIFxQpeoI4RWtJAB3UUzO4cCJhV1ADd6pBiBYQF4Ix8piPcGyIB6a" crossorigin="anonymous">
+    <style>
+      body, html {
+        height: 100%;
+      }
+      .cover-container {
+        max-width: 42em;
+      }
+    </style>
+   
+
+</head>
+<body>
+    
+</body>
+</html>
