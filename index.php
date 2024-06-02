327327c475acd1bec82e2c0f80bdb112f43ac9ea
emily <emymi2007@gmail.com>
2024-06-02 13:13:44 -0300

    botao de github adicionado na pagina index

--- index.php
+++ index.php
@@ -6,15 +6,6 @@
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ENjdO4Dr2bkBIFxQpeoI4RWtJAB3UUzO4cCJhV1ADd6pBiBYQF4Ix8piPcGyIB6a" crossorigin="anonymous">
-    <style>
-      body, html {
-        height: 100%;
-      }
-      .cover-container {
-        max-width: 42em;
-      }
-    </style>
    
 
 </head>
@@ -34,6 +25,7 @@
       <main class="px-3">
         <h1>Bem-vindo à aplicação PHP.</h1>
         <p class="lead">Esta é a página principal. Utilize o menu acima para navegar.</p>
+        <a href="https://github.com/emilypessoa" class="btn btn-primary">GitHub</a>
       </main>
 
       <footer class="mt-auto text-white-50">
