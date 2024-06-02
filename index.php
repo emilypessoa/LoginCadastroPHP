b38d333f478c9cf2546fab0ffe152b63e7859b71
emily <emymi2007@gmail.com>
2024-06-02 13:12:01 -0300

    body adicionado na pagina index

--- index.php
+++ index.php
@@ -18,7 +18,28 @@
    
 
 </head>
-<body>
-    
-</body>
+<body class="d-flex h-100 text-center text-bg-dark">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="mb-auto">
+        <div>
+          <h3 class="float-md-start mb-0">Aplicação PHP</h3>
+          <nav class="nav nav-masthead justify-content-center float-md-end">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link" href="login.php">Login</a>
+            <a class="nav-link" href="cadastro.php">Cadastro</a>
+          </nav>
+        </div>
+      </header>
+
+      <main class="px-3">
+        <h1>Bem-vindo à aplicação PHP.</h1>
+        <p class="lead">Esta é a página principal. Utilize o menu acima para navegar.</p>
+      </main>
+
+      <footer class="mt-auto text-white-50">
+        <p>Aplicação PHP com Bootstrap 5, by <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>.</p>
+      </footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+9b5U5fR5z0d3N5XyFw5p6C5MQf6D" crossorigin="anonymous"></script>
+  </body>
 </html>
